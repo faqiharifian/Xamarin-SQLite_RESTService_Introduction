628b5963e6a3167f293a501345e84739cdb7eb3e
Faqih Arifian <faqiharifianaji@gmail.com>
2017-06-11 12:53:52 +0700

    practice 1 employee list app using SQLite

---
+++ Modul5 Practice/Modul5_Practice.Android/SqliteService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using System.IO;
+using Xamarin.Forms;
+using Modul5_Practice.Droid;
+using Modul5_Practice;
+
+[assembly: Dependency(typeof(SqliteService))]
+namespace Modul5_Practice.Droid
+{
+    class SqliteService : ISQLite
+    {
+        public SqliteService() { }
+
+        public SQLite.Net.SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "Pegawai.db3";
+            string documentsPath =
+                   Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            Console.WriteLine(path);
+            if (!File.Exists(path)) File.Create(path);
+            var plat = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
+            var conn = new SQLite.Net.SQLiteConnection(plat, path);
+            return conn;
+        }
+    }
+}
---
+++ Modul5 Practice/Modul5_Practice/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite.Net.Attributes;
+
+namespace Modul5_Practice
+{
+    public class Employee
+    {
+        [PrimaryKey, AutoIncrement]
+        public long EmpId { get; set; }
+        [NotNull]
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public string Qualification { get; set; }
+    }
+}
